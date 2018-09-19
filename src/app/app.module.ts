ad71513f5c0cdca18ea9fa456fd4c84619a5d31c
Frederic Yesid Peña Sánchez <fredericpena@hotmail.com>
2018-09-18 19:24:23 -0500

    Setup Mongoose * Services, Schemas and models added * Controller added * Need to test why had to use forwardRef for BookService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { bookSchema } from './schemas/book.schema';
+import { BookService } from './services/book.service';
+import { BookController } from './controllers/book.controller';
 
 @Module({
-  controllers: [AppController]
+  controllers: [AppController, BookController],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nestjs-workshop-2018', {useNewUrlParser: true}),
+    MongooseModule.forFeature([
+      {name: 'tokenBookSchema', schema: bookSchema}
+    ])
+  ],
+  providers: [
+    BookService
+  ]
 })
 export class AppModule {
 }
---
+++ src/app/services/book.service.ts
@@ -0,0 +1,24 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { IBook } from '../interfaces/book.interface';
+
+
+@Injectable()
+export class BookService {
+  constructor(
+    @InjectModel('tokenBookSchema')
+    private readonly model: Model<IBook>
+  ) {
+  }
+
+  public find(conditions) {
+    return this.model.find(conditions).exec();
+  }
+
+  public create(book) {
+    const newBook = new this.model(book);
+    newBook.save();
+  }
+
+}
